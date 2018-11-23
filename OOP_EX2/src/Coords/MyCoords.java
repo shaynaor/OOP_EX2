ed4fcb8b010fe637860e3ca42ae1828d3acfb9a1
shaynaor <44779960+shaynaor@users.noreply.github.com>
2018-11-23 17:15:52 +0200

    MyCoords

--- OOP_EX2/src/Coords/MyCoords.java
+++ OOP_EX2/src/Coords/MyCoords.java
@@ -1,5 +1,32 @@
 package Coords;
 
+import Geom.Point3D;
+
 public class MyCoords implements coords_converter {
+
+	public Point3D add(Point3D gps, Point3D local_vector_in_meter) {
+		
+		return null;
+	}
+
+	public double distance3d(Point3D gps0, Point3D gps1) {
+		
+		return 0;
+	}
+
+	public Point3D vector3D(Point3D gps0, Point3D gps1) {
+		
+		return null;
+	}
+
+	public double[] azimuth_elevation_dist(Point3D gps0, Point3D gps1) {
+		
+		return null;
+	}
+
+	public boolean isValid_GPS_Point(Point3D p) {
+		
+		return false;
+	}
 
 }
