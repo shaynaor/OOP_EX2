1ec4a0779821c971a1a8107df72b63637c75a121
shaynaor <44779960+shaynaor@users.noreply.github.com>
2018-11-24 18:33:37 +0200

    adds to mycoords :  boolean isValid_GPS_Point  Point3D vector3D

--- OOP_EX2/src/Coords/MyCoords.java
+++ OOP_EX2/src/Coords/MyCoords.java
@@ -21,18 +21,37 @@ public class MyCoords implements coords_converter {
 	public Point3D vector3D(Point3D gps0, Point3D gps1) {
 		Point3D p0 = new Point3D(ConvertGpsToXyz.lla2ecef(gps0));
 		Point3D p1 = new Point3D(ConvertGpsToXyz.lla2ecef(gps1));
-		
-		return null;
+
+		double x = p1.x() - p0.x();
+		double y = p1.y() - p0.y();
+		double z = p1.z() - p0.z();
+
+		Point3D p0Top1 = new Point3D(x, y, z);
+
+		return p0Top1;
 	}
 
 	public double[] azimuth_elevation_dist(Point3D gps0, Point3D gps1) {
-		
+
 		return null;
 	}
 
 	public boolean isValid_GPS_Point(Point3D p) {
-		
-		return false;
+		if (p == null)
+			return false;
+
+		double lat = p.x(), lon = p.y(), alt = p.z();
+
+		if (lat < -90 && lat > 90)
+			return false;
+
+		if (lon < -180 && lon > 180)
+			return false;
+
+		if (alt < -450)
+			return false;
+
+		return true;
 	}
 
 }
