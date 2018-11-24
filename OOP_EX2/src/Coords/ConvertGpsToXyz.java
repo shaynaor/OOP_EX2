d84e43d365f71a64a2c99bd23f6e4610140339a4
shaynaor <44779960+shaynaor@users.noreply.github.com>
2018-11-24 17:04:15 +0200

    Adds  Point3D add distance3d

--- OOP_EX2/src/Coords/ConvertGpsToXyz.java
+++ OOP_EX2/src/Coords/ConvertGpsToXyz.java
@@ -2,15 +2,32 @@ package Coords;
 
 import java.util.Arrays;
 import Geom.Point3D;
+/**
+ * This class converts a Decimal degrees latitude longitude altitude point, into a ECEF(earth-centered, earth-fixed) 
+ * x,y,z point and the other way around.
+ *  ECEF wiki link: https://en.wikipedia.org/wiki/ECEF
+ *
+ * @author Alex Vaisman , Shay naor
+ *
+ */
+
 
 public class ConvertGpsToXyz {
 
-	public static final double a = 6378137;
-	public static final double f = 1 / 298.257223563;
+	public static final double a = 6378137; //the equatorial earth radius.
+	public static final double f = 1 / 298.257223563; // WGS84 flattening wiki link: https://en.wikipedia.org/wiki/Flattening
 	public static final double b = a * (1 - f);
 	public static final double e = Math.sqrt((Math.pow(a, 2) - Math.pow(b, 2)) / Math.pow(a, 2));
 	public static final double e2 = Math.sqrt((Math.pow(a, 2) - Math.pow(b, 2)) / Math.pow(b, 2));
 
+	
+	
+	/**
+	 * converting a ECEF x,y,z point in meters to lla (latitude longitude altitude)point.
+	 * The formula taken from : https://stackoverflow.com/questions/18253546/ecef-to-lla-lat-lon-alt-in-java.
+	 * @param vec the x,y,z point the function receives.
+	 * @return lla point
+	 */
 	public static Point3D ecef2lla(Point3D vec) {
 
 		double lat, lon, height, N, theta, p;
@@ -20,7 +37,7 @@ public class ConvertGpsToXyz {
 		theta = Math.atan((z * a) / (p * b));
 		lon = Math.atan(y / x);
 		lat = Math.atan(((z + Math.pow(e2, 2) * b * Math.pow(Math.sin(theta), 3))
-				/ ((p - Math.pow(e, 2) * a * Math.pow(Math.cos(theta), 3)))));
+		/ ((p - Math.pow(e, 2) * a * Math.pow(Math.cos(theta), 3)))));
 		N = a / (Math.sqrt(1 - (Math.pow(e, 2) * Math.pow(Math.sin(lat), 2))));
 
 		double m = (p / Math.cos(lat));
@@ -33,6 +50,12 @@ public class ConvertGpsToXyz {
 		return point;
 	}
 
+	/**
+	 * Converts a lla point into a ECEF x,y,z point.
+	 * The formula taken from : http://mathforum.org/library/drmath/view/51832.html.
+	 * @param vec The lla point the function receives.
+	 * @return ECEF point.
+	 */
 	public static Point3D lla2ecef(Point3D vec) {
 		
 		double lat = vec.x(), lon = vec.y(), h = vec.z();
@@ -59,6 +82,8 @@ public class ConvertGpsToXyz {
 		Point3D pointEcef = new Point3D ( lla2ecef(pointLla));
 		Point3D pointLlaNew = new Point3D (ecef2lla(pointEcef));
 		System.out.println(pointLlaNew);
+		
+		
 		
 		
 	}
--- OOP_EX2/src/Coords/MyCoords.java
+++ OOP_EX2/src/Coords/MyCoords.java
@@ -5,16 +5,22 @@ import Geom.Point3D;
 public class MyCoords implements coords_converter {
 
 	public Point3D add(Point3D gps, Point3D local_vector_in_meter) {
-		
-		return null;
+		Point3D p = new Point3D(ConvertGpsToXyz.lla2ecef(gps));
+		p.add(local_vector_in_meter);
+		p = ConvertGpsToXyz.ecef2lla(p);
+		return p;
 	}
 
 	public double distance3d(Point3D gps0, Point3D gps1) {
-		
-		return 0;
+		Point3D p0 = new Point3D(ConvertGpsToXyz.lla2ecef(gps0));
+		Point3D p1 = new Point3D(ConvertGpsToXyz.lla2ecef(gps1));
+		double distance = p0.distance3D(p1);
+		return distance;
 	}
 
 	public Point3D vector3D(Point3D gps0, Point3D gps1) {
+		Point3D p0 = new Point3D(ConvertGpsToXyz.lla2ecef(gps0));
+		Point3D p1 = new Point3D(ConvertGpsToXyz.lla2ecef(gps1));
 		
 		return null;
 	}
