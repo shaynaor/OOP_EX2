2896c60967f45f855d3f784597f7af16ed8fb1ab
shaynaor <44779960+shaynaor@users.noreply.github.com>
2018-11-29 14:07:05 +0200

    add few functions to Layer

--- OOP_EX2/src/GIS/Layer.java
+++ OOP_EX2/src/GIS/Layer.java
@@ -8,87 +8,60 @@ public class Layer implements GIS_layer {
     private HashSet<GIS_element> layer;
 	
 	
-	@Override
+    
 	public boolean add(GIS_element arg0) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.layer.add(arg0);
 	}
 
-	@Override
 	public boolean addAll(Collection<? extends GIS_element> arg0) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.layer.addAll(arg0);
 	}
 
-	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-
+		this.layer.clear();
 	}
 
-	@Override
 	public boolean contains(Object arg0) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.layer.contains(arg0);
 	}
 
-	@Override
 	public boolean containsAll(Collection<?> arg0) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.layer.containsAll(arg0);
 	}
 
-	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return this.layer.isEmpty();
 	}
 
-	@Override
 	public Iterator<GIS_element> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.layer.iterator();
 	}
 
-	@Override
 	public boolean remove(Object arg0) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.layer.remove(arg0);
 	}
 
-	@Override
 	public boolean removeAll(Collection<?> arg0) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.layer.removeAll(arg0);
 	}
 
-	@Override
 	public boolean retainAll(Collection<?> arg0) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.layer.retainAll(arg0);
 	}
 
-	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.layer.size();
 	}
 
-	@Override
 	public Object[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.layer.toArray();
 	}
 
-	@Override
 	public <T> T[] toArray(T[] arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.layer.toArray(arg0);
 	}
 
-	@Override
 	public Meta_data get_Meta_data() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
