f0dc0037d1c1d5fda0ebed922b84e948b5bb0f9a
shaynaor <44779960+shaynaor@users.noreply.github.com>
2018-11-29 10:12:54 +0200

    add test to isValid_GPS_Point

--- OOP_EX2/src/Coords/MyCoords.java
+++ OOP_EX2/src/Coords/MyCoords.java
@@ -61,17 +61,20 @@ public class MyCoords implements coords_converter {
 		ans[2] = distance;
 		return ans;
 	}
-
+	
+	/**
+	 * 
+	 */
 	public boolean isValid_GPS_Point(Point3D p) {
 		if (p == null)
 			return false;
 
 		double lat = p.x(), lon = p.y(), alt = p.z();
 
-		if (lat < -90 && lat > 90)
+		if (lat < -90 || lat > 90)
 			return false;
 
-		if (lon < -180 && lon > 180)
+		if (lon < -180 || lon > 180)
 			return false;
 
 		if (alt < -450)
