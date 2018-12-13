21e6ebd012d253d65134dd62982fd781a487af48
shaynaor <44779960+shaynaor@users.noreply.github.com>
2018-12-13 12:07:25 +0200

    create Fruit class

--- OOP_EX2/src/GIS/Fruit.java
+++ OOP_EX2/src/GIS/Fruit.java
@@ -1,5 +1,47 @@
 package GIS;
 
-public class Fruit {
+import java.text.ParseException;
+
+import Coords.MyCoords;
+import Geom.Geom_element;
+import Geom.Point3D;
+
+public class Fruit implements GIS_element {
+	private Point3D gps;
+	private Meta_data metaData;
+
+	public Fruit(String[] line) throws ParseException {
+		double lat = 0, lon = 0, alt = 0;
+		lat = Double.parseDouble(line[2]);
+		lon = Double.parseDouble(line[3]);
+		alt = Double.parseDouble(line[4]);
+		this.gps = new Point3D(lat, lon, alt);
+
+		this.metaData = new Mdata_game(line);
+	}
+
+	public Point3D getGps() {
+		return gps;
+	}
+
+	public Meta_data getMetaData() {
+		return metaData;
+	}
+
+	public Geom_element getGeom() {
+		return this.gps;
+	}
+
+	public Meta_data getData() {
+		return this.metaData;
+	}
+
+	/**
+	 * Transforms a gps point by a vector point in meters.
+	 */
+	public void translate(Point3D vec) {
+		MyCoords cor = new MyCoords();
+		this.gps = cor.add(this.gps, vec);
+	}
 
 }
