b08d7fe888b2d804240a9697d6fb6e76a69222a7
A Beast <alex.vaisman1@gmail.com>
2018-12-13 11:56:18 +0200

    added pacman and metadata game class

---
+++ OOP_EX2/src/GIS/Mdata_game.java
@@ -0,0 +1,39 @@
+package GIS;
+
+import java.text.ParseException;
+
+import Geom.Point3D;
+
+public class Mdata_game implements Meta_data {
+    private int id;
+    private double speedWeight;
+    private double radius;
+    
+	
+	public Mdata_game(String[] meta) throws ParseException {
+		this.id = Integer.parseInt(meta[1]);
+		this.speedWeight = Double.parseDouble(meta[5]);
+		this.radius = Double.parseDouble(meta[6]);
+		
+		
+		
+		
+		
+		
+	}
+
+
+	@Override
+	public long getUTC() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public Point3D get_Orientation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
