874836701fb87a05f15491d93fe33ea65627afcd
A Beast <alex.vaisman1@gmail.com>
2018-12-19 16:35:34 +0200

    added Javadoc to Range

--- OOP_EX2/src/Coords/Range.java
+++ OOP_EX2/src/Coords/Range.java
@@ -8,9 +8,21 @@ import Geom.Point3D;
 public class Range {
 
 	public Range( ) {
-		
 	}
 
+	
+	/**
+	 * This function receives two Fruits and a given time.
+	 * The points must have time associated to them.
+	 * The function will return the position of a point on a line between the two given fruits
+	 * for a certain time.
+	 * The time given must be greater then the time of a and lower then the time of b.
+	 * 
+	 * @param time ,The time for which a point must be found.
+	 * @param a , The first point.
+	 * @param b , The second point.
+	 * @return , a point between point a and b realtive to the given time.
+	 */
 	public Point3D getPosInTime(double time , Fruit a, Fruit b) {
 
 		double n = (time-a.getTimeEaten()) / (b.getTimeEaten()-a.getTimeEaten());
