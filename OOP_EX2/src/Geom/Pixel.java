b65017c39e164e2baa111dcec458e6b455d07c2a
A Beast <alex.vaisman1@gmail.com>
2018-12-19 16:44:27 +0200

    Added java doc to pixel

--- OOP_EX2/src/Geom/Pixel.java
+++ OOP_EX2/src/Geom/Pixel.java
@@ -1,5 +1,10 @@
 package Geom;
-
+/**
+ * This class represents a pixel
+ * This class has method to find distance between two pixels and a method
+ * to find the angle in degrees between them.
+ * @author Alex vaisman, Shay naor.
+ */
 public class Pixel {
 	private int x;
 	private int y;
@@ -7,17 +12,15 @@ public class Pixel {
 	public Pixel(int x, int y) {
 		this.x = x;
 		this.y = y;
-
 	}
 
-	public int getX() {
-		return x;
-	}
 
-	public int getY() {
-		return y;
-	}
-
+	/**
+	 * This function finds the distance between two pixels.
+	 * @param a , first pixel
+	 * @param b , second pixel
+	 * @return , the distance between the two in pixels.
+	 */
 	public double distancePixel(Pixel a, Pixel b) {
 		double x1 = a.getX();
 		double x2 = b.getX();
@@ -28,7 +31,12 @@ public class Pixel {
 		distance = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
 		return distance;
 	}
-
+	/**
+	 * This function find the angle between pixel a to pixel b in degrees.
+	 * @param a , the first pixel
+	 * @param b , the second pixel
+	 * @return the angel in degrees between a and b.
+	 */
 	public double anglePixel(Pixel a, Pixel b) {
 		double dist = distancePixel(a, b);
 		double dx;
@@ -42,6 +50,13 @@ public class Pixel {
 	public String toString() {
 		return "("+ x + "," + y + ")";
 	}
-	
 
+	//=======Getters=========
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
 }
