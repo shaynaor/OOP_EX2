ad823a2de746ab09c74b9dcbe037a05b1f4338bf
A Beast <alex.vaisman1@gmail.com>
2018-12-19 16:57:22 +0200

    Added Javadoc to Fruit

--- OOP_EX2/src/GIS/Fruit.java
+++ OOP_EX2/src/GIS/Fruit.java
@@ -5,7 +5,15 @@ import java.text.ParseException;
 import Coords.MyCoords;
 import Geom.Geom_element;
 import Geom.Point3D;
-
+/**
+ * This class represents a Fruit.
+ * A fruit is an object with a gps point associated to it,
+ * a score , how much its worth when eaten.
+ * an id, a boolean eaten flag to tell if it was eaten or not.
+ * and a timer to tell when it was eaten.
+ * @author Alex Vaisman ,Shay naor.
+ *
+ */
 public class Fruit implements GIS_element {
 	private Point3D gps;
 	private Mdata_game metaData;
@@ -13,7 +21,11 @@ public class Fruit implements GIS_element {
 	private double timeEaten;
 
 
-
+   /**
+    * Fruit Constructor receives a line from a CSV file and creates a fruit from the given data.
+    * @param line ,  a line from CSV file.
+    * @throws ParseException , Throws ParseException if CSV data is incorrect.
+    */
 	public Fruit(String[] line) throws ParseException {
 		double lat = 0, lon = 0, alt = 0;
 		lat = Double.parseDouble(line[2]);
@@ -24,13 +36,21 @@ public class Fruit implements GIS_element {
 		this.metaData = new Mdata_game(line);
 	}
 	
+	/**
+	 * Fruit constructor , creates a fruit from gps lat and lon.
+	 * @param x , the lat value.
+	 * @param y , the lon value.
+	 * @param id , the id of the fruit.
+	 */
 	public Fruit(double x, double y, int id) {
 		double lat = x, lon = y, alt = 0;
 		this.gps = new Point3D(lat, lon, alt);
 		
 		this.metaData = new Mdata_game(id, false);
 	}
-
+    
+	
+	//======Getters=======
 	public Point3D getGps() {
 		return gps;
 	}
