748e5d312077e54481dddf64be4eceabdf22f0ad
A Beast <alex.vaisman1@gmail.com>
2018-12-19 17:26:40 +0200

    Added java doc for Path class

--- OOP_EX2/src/GIS/Path.java
+++ OOP_EX2/src/GIS/Path.java
@@ -2,6 +2,14 @@ package GIS;
 
 import java.util.ArrayList;
 
+/**
+ * This class represents a Path.
+ * A path is an ArrayList of GIS_elements.
+ * A path will be arranged to represent the path a pacman must take to eat all the fruits.
+ * 
+ * @author Alex vaisman, Shay naor.
+ *
+ */
 public class Path {
 
 
