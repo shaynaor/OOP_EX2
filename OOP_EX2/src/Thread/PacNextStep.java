5c1168ddd6b959f447c6f3b914a13c4593d1a20d
A Beast <alex.vaisman1@gmail.com>
2018-12-23 13:28:41 +0200

    Added java doc

--- OOP_EX2/src/Thread/PacNextStep.java
+++ OOP_EX2/src/Thread/PacNextStep.java
@@ -13,6 +13,14 @@ import GIS.Solution;
 import Geom.Point3D;
 import Gui.MyFrame;
 
+/**
+ * This class is a thread which finds the position of the pacmans 
+ * on the route from their starting position to the last fruit.
+ * the pacmans will be following the path which is visible in the gui.
+ * the thread will run till it reaches the time of the longest path , or its told to shutdown.
+ * @author Alex vaisman, Shay naor
+ *
+ */
 public class PacNextStep implements Runnable {
 
 	private MyFrame frame;
@@ -22,7 +30,16 @@ public class PacNextStep implements Runnable {
 		this.frame = frame;
 
 	}
-
+/**
+ * This function will update the position of all the pacmans in the solution 
+ * and will put them into a Vector.
+ * after putting them in a Vector the function will call a set function from MyFrame and that will cause the gui
+ * to update.
+ * after that the thread will sleep for some time.
+ * during the run time of the gui the speed can be changed within the gui and will be updated while
+ * the thread is running.
+ * 
+ */
 	public void run() {
 		double currentTime = this.frame.getCurrentTime();
 		double endTime = this.frame.getAlgo().getSolution().getTime();
@@ -33,7 +50,8 @@ public class PacNextStep implements Runnable {
 		Point3D point = new Point3D(0, 0, 0);
        
 
-
+       //will find the next location of the pacmans for current time
+	  //if current time goes longer then the longest path the function will stop.
 		while (currentTime <= endTime) {
 			if(this.frame.isKill()==true) {// this kills the thread 
 				currentTime = endTime + 50;
@@ -60,15 +78,27 @@ public class PacNextStep implements Runnable {
 			}
 			this.frame.setNextPacman(pacmans);
 			pathIt = frame.getAlgo().getSolution().getSolution().iterator();
-			currentTime += 0.5;        ///////////////////////////PLAYED WTH THIS TWO
+			currentTime += 0.5;        
 			try {
-				Thread.sleep(this.frame.getSpeed());   /////////////////////////////////////
+				Thread.sleep(this.frame.getSpeed());   
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
 	}
 
+	/**
+	 * This function finds for a given time two fruits.
+	 * It will find the fruits using the time eaten.
+	 * the first fruit will be less or equal to the given time.
+	 * the second fruit will be more or equal to the given time.
+	 * if the first fruit is a pacman the function will convert it to a fruit.
+	 * This function exist to make sure that range receives correct values.
+	 * 
+	 * @param time ,  the time given.
+	 * @param path ,  the path with the fruits.
+	 * @return two fruits bounding the time.
+	 */
 	public Fruit[] findBounds(double time, Path path) {
 		Fruit[] fruitsArr = new Fruit[2];
 
